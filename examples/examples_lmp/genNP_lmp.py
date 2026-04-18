4c61298874ea635c6824b0f058dc1ad33fd4985f
nanogchen-ibupower <nanogchen@ibuypower>
2026-04-18 11:10:12 -0500

    corrected lmp example

--- examples/examples_lmp/genNP_lmp.py
+++ examples/examples_lmp/genNP_lmp.py
@@ -1,7 +1,7 @@
 #!/bin/env python
 
 import sys
-sys.path.insert(0,"../polyGraft/")
+sys.path.insert(0,"../../src/")
 from polyGraft import polyGraft
 from polymer import Polymer
 from crystal import Crystal
@@ -16,9 +16,9 @@ if __name__ == '__main__':
 	peo.readDATA("PEO20.data", atom_style="id resid type charge x y z")
 
 	# read Au nanoparticle or by generation (the following two lines)
-	# lattice = Atomsk(lattice_type='fcc', lattice_const=4.08, element='Au')
-	# lattice.gen_particle(radius, outFile="AuNP-R20.data")
 	radius = 20.0
+	lattice = Atomsk(lattice_type='fcc', lattice_const=4.08, element='Au')
+	lattice.gen_particle(radius, outFile="AuNP-R20.data")	
 	nanoparticle = Crystal("nanoparticle", 'Au', radius)
 	nanoparticle.readDATA("AuNP-R20.data",atom_style="id type x y z", guessing_bond=True, lattice_const=4.08)
 
